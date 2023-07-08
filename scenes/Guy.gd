526627ba2ca62dda509ca4ac4970ae3e1874ab97
T <tabutyn@gmail.com>
2023-07-08 19:16:33 -0230

    add a cute little guy and hook it up

--- scenes/Guy.gd
+++ scenes/Guy.gd
@@ -7,21 +7,27 @@ var MOVEMENT_INERTIA = 10
 var wiggle_velocity = Vector2(0, 0)
 var movement_velocity = Vector2(0, 0)
 var velocity = Vector2(0, 0)
+var animation_float = 0.0
 
 var dead = false
 var dispose = false
 var won = false
 
 func _ready():
-	pass
+	animation_float = rand_range(0.0, 0.25)
+
+func _process(delta):
+	animation_float += delta
+	$Icon.frame = int(animation_float / 0.25) % 4
 
 func _physics_process(delta):
 	if not dead:
+		animation_float += delta * (velocity.length() / 200.0)
+
 		wiggle_velocity.x = (wiggle_velocity.x*WIGGLE_INERTIA + rand_range(-INTENSITY, INTENSITY)) / (WIGGLE_INERTIA+1)
 		wiggle_velocity.y = (wiggle_velocity.y*WIGGLE_INERTIA + rand_range(-INTENSITY, INTENSITY)) / (WIGGLE_INERTIA+1)
 		
 		velocity = (velocity*MOVEMENT_INERTIA + movement_velocity) / (MOVEMENT_INERTIA+1)
-		
 		move_and_slide(velocity + wiggle_velocity)
 		
 		for i in get_slide_count():
