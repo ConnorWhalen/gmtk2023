517012bef14c3397d7c3f8eb6f9b2957a09eb0d1
ConnorWhalen <caw742@mun.ca>
2023-07-08 22:56:57 -0230

    add bg music and guy sfx

--- scenes/Guy.gd
+++ scenes/Guy.gd
@@ -1,5 +1,15 @@
 extends KinematicBody2D
 
+var SFX_FILES = [
+	"gmtk2023_1#01.wav",
+	"gmtk2023_1#03.3.wav",
+	"gmtk2023_1#03.7.wav",
+	"gmtk2023_1#03.11.wav",
+	"gmtk2023_1#03.12.wav",
+	"gmtk2023_1#03.15.wav",
+	"gmtk2023_1#03.18.wav",
+]
+
 var INTENSITY = 100
 var WIGGLE_INERTIA = 4
 var MOVEMENT_INERTIA = 10
@@ -13,12 +23,22 @@ var dead = false
 var dispose = false
 var won = false
 
+var sfx_node
+var sfx_cheer_node
+
 func _ready():
 	animation_float = rand_range(0.0, 0.25)
+	
 
 func _process(delta):
 	animation_float += delta
 	$Icon.frame = int(animation_float / 0.25) % 4
+	
+	var sfx_index = randi() % 7
+	sfx_node = get_node("SFXPlayer" + str(sfx_index))
+	
+	var sfx_cheer_index = randi() % 6
+	sfx_cheer_node = get_node("SFXCheerPlayer" + str(sfx_cheer_index))
 
 func _physics_process(delta):
 	if not dead:
@@ -44,6 +64,8 @@ func add_movement(velocity: Vector2):
 	movement_velocity += velocity
 
 func hit():
+	sfx_node.play()
+	
 	animation_float = 0.0
 	dead = true
 	$Icon.visible = false
@@ -52,4 +74,6 @@ func hit():
 	
 
 func win():
+	sfx_cheer_node.play()
 	won = true
+	
