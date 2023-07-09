5aa583273b7184df12bc070537cb79cf17513b02
ConnorWhalen <caw742@mun.ca>
2023-07-09 13:47:40 -0230

    cull bullets when they're out of bounds

--- scenes/BigBullet.gd
+++ scenes/BigBullet.gd
@@ -11,6 +11,10 @@ func _physics_process(delta):
 	position += velocity * delta
 	for i in get_slide_count():
 		var collision = get_slide_collision(i)
+		
+	if (global_position.x > 6000 or global_position.x < -3000 or 
+		global_position.y > 2000 or global_position.y < -5000):
+		collided = true
 
 func hit():
 	pass
--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -12,6 +12,10 @@ func _physics_process(delta):
 	
 	for i in get_slide_count():
 		var collision = get_slide_collision(i)
+	
+	if (global_position.x > 6000 or global_position.x < -3000 or 
+		global_position.y > 2000 or global_position.y < -5000):
+		collided = true
 
 func hit():
 	collided = true
